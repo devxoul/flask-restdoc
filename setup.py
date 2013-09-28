f87a8937c3187852229e35b2c573964360470482
전수열 <devxoul@gmail.com>
2013-09-28 23:19:55 +0900

    v0.0.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'Flask-Restdoc',
+    packages = ['Flask-Restdoc'],
+    version = '0.0.1',
+    description = 'Flask-Restdoc is a simple tool that generates RESTful API documentation automatically from python files.',
+    author = 'Su Yeol Jeon',
+    author_email = 'devxoul@gmail.com',
+    url = 'https://github.com/devxoul/flask-restdoc',
+    download_url = 'https://github.com/peterldowns/mypackage/tarball/0.0.1',
+    keywords = ['Flask', 'REST', 'API', 'Documentation'],
+    classifiers = [],
+)
